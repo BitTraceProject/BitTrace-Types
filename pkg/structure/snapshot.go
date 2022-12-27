0a5fc668724aca970c196dc800c44a11971f6085
ZJH <zjh98@vip.qq.com>
2022-12-27 21:18:16 +0800

    update snapshot

--- pkg/structure/snapshot.go
+++ pkg/structure/snapshot.go
@@ -6,35 +6,42 @@ type (
 	// Snapshot 代表一次完整的区块同步流程，主要由一系列的 Revision 组成，
 	// 输出两次，初始化一次，结束一次，之间所有 ID 一致的 Revision 都属于这个 Snapshot
 	Snapshot struct {
-		ID                string     `json:"id"` // chain id + chain height + init timestamp
-		TargetChainID     string     `json:"target_chain_id"`
-		TargetChainHeight int32      `json:"target_chain_height"`
-		InitTimestamp     Timestamp  `json:"init_timestamp"`
-		FinalTimestamp    Timestamp  `json:"final_timestamp"`
-		InitStatus        Status     `json:"init_status"`
-		FinalStatus       Status     `json:"final_status"`  // 这个其实不是必要的，可以用于核对 status transfer list 的操作结果是否正确
-		RevisionList      []Revision `json:"revision_list"` // 输出的时候不会输出这个，也不会维护它（在结构外维护一个 List），这个是逻辑上的关系（由 resolver 处理得到）
+		ID                string       `json:"id"` // chain id + chain height + init timestamp
+		TargetChainID     string       `json:"target_chain_id"`
+		TargetChainHeight int32        `json:"target_chain_height"`
+		Type              SnapshotType `json:"type"`
+		Timestamp         Timestamp    `json:"timestamp"`
+		InitStatus        Status       `json:"init_status"`
+		FinalStatus       Status       `json:"final_status"`  // 这个其实不是必要的，可以用于核对 status transfer list 的操作结果是否正确
+		RevisionList      []Revision   `json:"revision_list"` // 输出的时候不会输出这个，也不会维护它（在结构外维护一个 List），这个是逻辑上的关系（由 resolver 处理得到）
 	}
+	SnapshotType int
 )
 
-func InitSnapshot(targetChainID string, targetChainHeight int32, initTime time.Time, initStatus Status) Snapshot {
-	initTimestamp := FromTime(initTime)
-	id := GenSnapshotID(targetChainID, targetChainHeight, initTimestamp)
+const (
+	SnapshotInit = iota
+	SnapshotFinal
+)
+
+func InitSnapshot(targetChainID string, targetChainHeight int32, t time.Time, initStatus Status) Snapshot {
+	timestamp := FromTime(t)
+	id := GenSnapshotID(targetChainID, targetChainHeight, timestamp)
 	s := Snapshot{
 		ID:                id,
 		TargetChainID:     targetChainID,
 		TargetChainHeight: targetChainHeight,
-		InitTimestamp:     initTimestamp,
-		FinalTimestamp:    0,
+		Type:              SnapshotInit,
+		Timestamp:         timestamp,
 		InitStatus:        initStatus,
 		RevisionList:      nil,
 	}
 	return s
 }
 
-func FinalSnapshot(s Snapshot, finalTime time.Time, finalStatus Status) Snapshot {
-	finalTimestamp := FromTime(finalTime)
-	s.FinalTimestamp = finalTimestamp
+func FinalSnapshot(s Snapshot, t time.Time, finalStatus Status) Snapshot {
+	timestamp := FromTime(t)
+	s.Type = SnapshotFinal
+	s.Timestamp = timestamp
 	s.FinalStatus = finalStatus
 	return s
 }
